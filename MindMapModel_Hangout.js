beaab27823ef12ddf0adfcc260d89d1a27871a81
Juan Carlos García Ramírez <enok7@hotmail.com>
2013-01-11 07:57:52 -0600

    Update MindMapModel_Hangout.js
    dd

--- MindMapModel_Hangout.js
+++ MindMapModel_Hangout.js
@@ -47,7 +47,7 @@ function pintaMapa(json) {
   //
   
   //eventBus.publish(mindmaps.Event.DOCUMENT_OPENED, mindmaps.Document.fromObject(doc));
-  //gapi.hangout.data.setValue("doc");
+  gapi.hangout.data.setValue("doc");
   //alert("Nose por que no pinto el mapa3");
   //this.cargaMapa2(json);
   
